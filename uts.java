28f8ce5dbf019fefdd9e9a135f39c89b99e2097c
aghniakfm2 <aghnia.kfm@gmail.com>
2026-04-22 10:29:02 +0700

    Update uts.java

--- uts.java
+++ uts.java
@@ -23,3 +23,28 @@ class Smartphone {
     public int getRam() {
         return ram;
     }
+    
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public void tampilData() {
+        System.out.println("Merk: " + merk +
+                           ", Harga: " + harga +
+                           ", RAM: " + ram + "GB");
+    }
+}
+
+// SUBCLASS
+class SmartphoneGaming extends Smartphone {
+    private int refreshRate;
+
+    SmartphoneGaming(String merk, double harga, int ram, int refreshRate) {
