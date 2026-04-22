3ba06a27dc15b8bd65e99308783984d7e59b99b4
GigihJ16 <gigihjuang36@gmail.com>
2026-04-22 10:26:09 +0700

    Add Smartphone class with attributes and methods

--- uts.java
+++ uts.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+// SUPERCLASS
+class Smartphone {
+    private String merk;
+    private double harga;
+    private int ram;
+
+    Smartphone(String merk, double harga, int ram) {
+        this.merk = merk;
+        this.harga = harga;
+        this.ram = ram;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public int getRam() {
+        return ram;
+    }
